cbb4f602aefddbfa81f2018458a0a5c2174a69b8
Vanja Kojuharova <vanja_kojuharova@harvard.edu>
2014-01-14 14:04:36 -0500

    IDMAUTH-198 - Split git repository into modules

--- deploy/resources/modules/mod_auth_cas/manifests/params.pp
+++
@@ -1,7 +0,0 @@
-class mod_auth_cas::params (
-    $servername             = $apache::params::servername,
-    $confd_dir              = $apache::params::confd_dir,
-) inherits apache::params {
-
-    notice("DEBUG::mod_auth_cas::params:: servername is $servername")
-}
